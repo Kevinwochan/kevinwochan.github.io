a5d86adf8f709536896e6dc57d7a3fb193f942f3
kevinwochan <kevinwochan@gmail.com>
2024-02-21 20:05:55 +1100

    fix: using gatsby page props

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -11,7 +11,7 @@ const FullHeightRow = styled.div`
   height: 100vh;
 `;
 
-const Gallery = () => {
+const Gallery = ({ slide }) => {
   const data = useStaticQuery(graphql`
     query MyQuery {
       allFile(filter: {relativeDirectory: {eq: "events"}}) {
@@ -23,25 +23,27 @@ const Gallery = () => {
       }
     }
 `)
-  let params = new URLSearchParams(window?.location?.search || '');
-  const images = [
-    ...data.allFile.edges.filter(p => p.node.publicURL).map(p => ({ original: p.node.publicURL, thumbnail: p.node.publicURL }))
-  ];
-  console.log(params.get('slide'))
+
+  const images = data.allFile.edges
+    .filter(p => p.node.publicURL)
+    .map(p => ({ original: p.node.publicURL, thumbnail: p.node.publicURL }));
+
   return (
     <section className="resume-section container">
       <FullHeightRow className="d-flex align-items-center justify-content-center">
-        <ImageGallery items={images} startIndex={Number(params.get('slide')) || 0} />
+        <ImageGallery items={images} startIndex={slide || 0} />
       </FullHeightRow>
     </section>
   );
 }
 
 
-const IndexPage = () => {
+// https://www.gatsbyjs.com/docs/location-data-from-props/
+const IndexPage = ({ location }) => {
+  const { slide } = new URLSearchParams(location?.search);
   return (
     <Layout>
-      <Gallery />
+      <Gallery slide={slide} />
     </Layout>
   )
 }
