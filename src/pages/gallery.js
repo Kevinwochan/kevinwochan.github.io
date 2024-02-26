c38310f96daa1c1b1fe070eb44d8b86c17b50213
kevinwochan <kevinwochan@gmail.com>
2024-02-26 15:41:27 +1100

    restory gallery page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import Layout from "../components/Layout"
+import styled from 'styled-components';
 import '../../node_modules/react-image-gallery/styles/css/image-gallery.css';
-import { Gallery } from '../components/Gallery';
+import { useStaticQuery, graphql } from "gatsby"
+import ImageGallery from "react-image-gallery";
+
+
+const FullHeightRow = styled.div`
+  overflow-y: hidden;
+  height: 100vh;
+`;
 
 // https://www.gatsbyjs.com/docs/location-data-from-props/
-export const GalleryPage = ({ location }) => {
+const IndexPage = ({ location }) => {
+  const [slide, setSlide] = useState();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location?.search);
+    setSlide(Number(params.get('slide')));
+  }, [])
+  const data = useStaticQuery(graphql`
+    query galleryQuery {
+      allFile(filter: {relativeDirectory: {eq: "events"}}) {
+        edges {
+          node {
+            publicURL
+          }
+        }
+      }
+    }
+`)
+
+  const images = data.allFile.edges
+    .sort((a, b) => a.node.publicURL - b.node.publicURL)
+    .filter(p => p.node.publicURL)
+    .map(p => ({ original: p.node.publicURL, thumbnail: p.node.publicURL }));
+
   return (
     <Layout>
-      <Gallery location={location} />
+      <section className="resume-section container">
+        <FullHeightRow className="d-flex align-items-center justify-content-center">
+          <ImageGallery items={images} startIndex={slide} />
+        </FullHeightRow>
+      </section>
     </Layout>
   )
 }
 
+export default IndexPage;
