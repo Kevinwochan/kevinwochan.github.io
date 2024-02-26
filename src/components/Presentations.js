7bd7ac053f8c0bbb9a34b910da7b0ace2d38ca1f
kevinwochan <kevinwochan@gmail.com>
2024-02-26 15:50:59 +1100

    added chrome theme

--- src/components/Presentations.js
+++ src/components/Presentations.js
@@ -34,6 +34,14 @@ export const Presentations = () => (
                 </iframe>
                 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEa1-Xvcn8&#x2F;-fWmZT5DODt9pLOXJi0O9w&#x2F;view?utm_content=DAEa1-Xvcn8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">How do locks work?</a>
             </div>
+            <div class="col-sm">
+                <iframe loading="lazy"
+                    className='d-block m-auto'
+                    style={{ minHeight: '30vh', minWidth: '30vw' }}
+                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbvD9bQFU&#x2F;VEyR1ecEVeHCrNtcBSCMmw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                </iframe>
+                <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbvD9bQFU&#x2F;VEyR1ecEVeHCrNtcBSCMmw&#x2F;view?utm_content=DAEbvD9bQFU&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Google Chrome Theme</a>
+            </div>
         </FullHeightRow>
     </section >
 );
