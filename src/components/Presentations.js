e58d64fa63921c7f52c0b520716117599b3421f8
kevinwochan <kevinwochan@gmail.com>
2024-02-26 17:53:33 +1100

    setting width of container

--- src/components/Presentations.js
+++ src/components/Presentations.js
@@ -12,7 +12,7 @@ export const Presentations = () => (
             <div class="col-sm" >
                 <iframe loading="lazy"
                     className='d-block m-auto'
-                    style={{ minHeight: '30vh', minWidth: '30vw' }}
+                    style={{ minHeight: '30vh', minWidth: '30%' }}
                     src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbmgjGUpk&#x2F;t_1Lz9oIWvRWgbl1mE0XNA&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
                 </iframe>
                 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbmgjGUpk&#x2F;t_1Lz9oIWvRWgbl1mE0XNA&#x2F;view?utm_content=DAEbmgjGUpk&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">PASSWORDS 101</a>
@@ -20,7 +20,7 @@ export const Presentations = () => (
             <div class="col-sm">
                 <iframe loading="lazy"
                     className='d-block m-auto'
-                    style={{ minHeight: '30vh', minWidth: '30vw' }}
+                    style={{ minHeight: '30vh', minWidth: '30%' }}
                     src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEeTzvJgN8&#x2F;17juJRNc7SFMl7pORtLpVg&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
                 </iframe>
                 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEeTzvJgN8&#x2F;17juJRNc7SFMl7pORtLpVg&#x2F;view?utm_content=DAEeTzvJgN8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Capture The Flag</a>
@@ -28,7 +28,7 @@ export const Presentations = () => (
             <div class="col-sm">
                 <iframe loading="lazy"
                     className='d-block m-auto'
-                    style={{ minHeight: '30vh', minWidth: '30vw' }}
+                    style={{ minHeight: '30vh', minWidth: '30%' }}
                     src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEa1-Xvcn8&#x2F;-fWmZT5DODt9pLOXJi0O9w&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
                 </iframe>
                 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEa1-Xvcn8&#x2F;-fWmZT5DODt9pLOXJi0O9w&#x2F;view?utm_content=DAEa1-Xvcn8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">How do locks work?</a>
@@ -36,7 +36,7 @@ export const Presentations = () => (
             <div class="col-sm">
                 <iframe loading="lazy"
                     className='d-block m-auto'
-                    style={{ minHeight: '30vh', minWidth: '30vw' }}
+                    style={{ minHeight: '30vh', minWidth: '30%' }}
                     src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbvD9bQFU&#x2F;VEyR1ecEVeHCrNtcBSCMmw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
                 </iframe>
                 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEbvD9bQFU&#x2F;VEyR1ecEVeHCrNtcBSCMmw&#x2F;view?utm_content=DAEbvD9bQFU&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Google Chrome Theme</a>
