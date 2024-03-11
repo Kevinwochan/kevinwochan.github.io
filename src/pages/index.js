4ba06f8f7b77bcafe534c138b54c24926b545dac
kevinwochan <kevinwochan@gmail.com>
2024-03-11 21:17:45 +0800

    added badges

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
-import Layout from "../components/Layout"
-import { StaticImage } from "gatsby-plugin-image"
-import styled from 'styled-components';
-import Typist from 'react-typist';
-import '../../node_modules/react-typist/dist/Typist.css'
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import Typist from "react-typist";
+import "../../node_modules/react-typist/dist/Typist.css";
 
 const IconButton = styled.a`
   display: inline-block;
@@ -20,7 +21,7 @@ const IconButton = styled.a`
   &:hover i {
     color: #81a1c1;
   }
-`
+`;
 
 const FullHeightRow = styled.div`
   overflow-y: hidden;
@@ -28,7 +29,26 @@ const FullHeightRow = styled.div`
 `;
 
 const About = () => {
-  const [msg, setMsg] = useState(null);
+  const [msg, setMsg] = useState("");
+  const data = useStaticQuery(graphql`
+    query badges {
+      allFile(filter: { relativeDirectory: { eq: "badges" } }) {
+        edges {
+          node {
+            publicURL
+          }
+        }
+      }
+    }
+  `);
+
+  const badgeUrls = data.allFile.edges
+    .sort((a, b) => a.node.publicURL - b.node.publicURL)
+    .filter((p) => p.node.publicURL)
+    .map((p) => p.node.publicURL);
+
+  console.log(badgeUrls);
+
   setTimeout(() => {
     setMsg("Pssst! You're still here?");
   }, 30000);
@@ -36,9 +56,28 @@ const About = () => {
   return (
     <section className="resume-section container">
       <FullHeightRow className="d-flex align-items-center justify-content-center">
-        <div className="d-none d-lg-block d-flex-item text-end">
-          <StaticImage layout="fixed" height={200} width={200} className="img-fluid img-profile rounded-circle" src="../images/kevin.jpeg" alt="Kevin Chan" />
-          {msg && (<h2 className="mt-2">{msg}</h2>)}
+        <div className="d-none d-lg-block d-flex-item">
+          <StaticImage
+            layout="fixed"
+            height={200}
+            width={200}
+            className="img-fluid img-profile rounded-circle"
+            src="../images/kevin.jpeg"
+            alt="Kevin Chan"
+          />
+          <div className="subheading icons text-left my-3">
+            <IconButton href="https://linkedin.com/in/kevinwochan/">
+              <i
+                className="bi bi-linkedin"
+                onClick={() => {
+                  setMsg("Talk to you soon....");
+                }}
+              />
+            </IconButton>
+            <IconButton href="https://github.com/Kevinwochan">
+              <i className="bi bi-github" />
+            </IconButton>
+          </div>
         </div>
         <div className="p-5 d-flex-item">
           <StaticImage layout="fixed" height={200} width={200} className="img-fluid img-profile rounded-circle mb-2 d-lg-none" src="../images/kevin.jpeg" alt="Kevin Chan"
@@ -53,18 +92,22 @@ const About = () => {
           <div className="lead mb-4">
             <p className="lead">
               <Typist>
-                Software Engineer interested in information security and software architectures.
+                Software Engineer interested in information security and
+                software architectures.
               </Typist>
             </p>
           </div>
           <div className="subheading icons ml-1">
-            <IconButton href="https://linkedin.com/in/kevinwochan/">
-              <i className="bi bi-linkedin" onClick={() => { setMsg('Talk to you soon....') }} />
-            </IconButton>
-            <IconButton href="https://github.com/Kevinwochan">
-              <i className="bi bi-github" />
-            </IconButton>
+            {badgeUrls.map((url) => (
+              <img
+                src={url}
+                className="m-2"
+                style={{ maxWidth: "20vw", maxHeight: "10vh" }}
+              />
+            ))}
           </div>
+
+          {msg && <Typist> {msg} </Typist>}
         </div>
       </FullHeightRow>
     </section>
