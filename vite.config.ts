3c694196f122aa60d2700f1c969d633d8d52a8bc
kevinwochan <kevinwochan@gmail.com>
2025-03-23 18:31:54 +1100

    added google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePluginRadar({ analytics: { id: "G-0SDL9ZF6SC" } })
+  ],
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
